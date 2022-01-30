700fabbf37d3654eb0693b4ca7d9d0312d4ba903
koushik global <koushikofficial@yahoo.com>
2022-01-30 14:35:57 +0000

    requimenst added

---
+++ setup.py
@@ -0,0 +1,24 @@
+#in order to make src directory global we create this file 
+
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Koushik V",
+    description="A small package for dvc ml pipeline    ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KoushikVK/Automate-MLPipelines-using-DVC",
+    author_email="koushikofficial@yahoo.com",
+    packages=["src"],
+    python_requires=">=3.7",
+    install_requires=[
+        'dvc',
+        'pandas',
+        'scikit-learn'
+    ]
+)
