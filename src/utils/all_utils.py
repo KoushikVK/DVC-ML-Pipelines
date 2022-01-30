0b62a44cc8af765c944ff9ae3875d56ff5945ace
koushik global <koushikofficial@yahoo.com>
2022-01-30 15:25:25 +0000

    requimenst added

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -1,11 +1,25 @@
+from numpy import True_
 import yaml
 import os
 
 
+#send config file and it ll return as dictionary
+
+#loads the yaml file
 
 def read_yaml(path_to_yaml: str)-> dict:  #gets an input as str and return dict
     with open(path_to_yaml) as yaml_file:
         content = yaml.safe_load(yaml_file)
 
 
-    return content    
+    return content  
+
+#creating a function for (Creating a dir)
+
+def create_directory(dirs:list):
+    for dir_path in dirs:
+        os.makedirs(dir_path,exist_ok=True)
+        print(f"dir is placed at {dir_path}")
+
+
+
